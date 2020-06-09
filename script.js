6713243efb37018e75ca466a7e7b78a36ac5b4e2
PaolaDM <orchidea.preziosa@gmail.com>
2020-06-09 22:53:57 +0100

    set showUser() to reteive user information
    retreive Github ID,  Profile Information, Avatar Image

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
       showUser(getGithubInfo(username));
     }
   });
+});
 
 
-});function getGithubInfo(username) {
+function getGithubInfo(username) {
   var url = "https://api.github.com/users/" + username;
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", url, false);
@@ -19,3 +20,40 @@ $(document).ready(function() {
 
   return xmlhttp;
 }
+
+
+function showUser(xmlhttp) {
+  if (xmlhttp.status===200) {
+    var json = xmlhttp.responseText;
+    var user = JSON.parse(json);
+
+
+    // 1. Github ID
+    document.getElementById("githubId").innerHTML = username + ' is GitHub user #' + user.id;
+
+    // 2. Profile information
+    var oldDiv = document.querySelector("#profile .information");
+
+    var newAnchor = document.createElement("a");
+    var newContent = document.createTextNode(" go to GitHub profile link");
+    newAnchor.href = user.html_url;
+    newAnchor.classList.add("profile");
+    newAnchor.appendChild(newContent);
+    oldDiv.appendChild(newAnchor);
+
+
+    // 3. Avatar image
+    var newImage = document.createElement("img");
+    var avatarUrl = user.avatar_url;
+    newImage.src = avatarUrl;
+    var avatarParents = document.getElementsByClassName("avatar");
+
+    if (avatarParents && avatarParents.length === 1) {
+      var avatarParent = avatarParents[0];
+      avatarParent.appendChild(newImage);
+    }}
+
+  var input = document.getElementById("username");
+  input.value = "";
+}
+
