083416547590ac53d4679d7fd97fc909e8426bb9
PaolaDMadd <44216565+PaolaDMadd@users.noreply.github.com>
2020-06-09 22:29:10 +0100

    starting files

---
+++ script.js
@@ -0,0 +1,11 @@
+//github user finder example
+var username = "";
+$(document).ready(function() {
+  $(document).on("keypress", "#username", function(event) {
+    if (event.which === 13) {
+      var input = $(this);
+    username = input.val();
+      showUser(getGithubInfo(username));
+    }
+  });
+});
